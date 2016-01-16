47a766df2bf395df696e3fe895196a61821cf62b
jerryli27 <jerrylijiaming@gmail.com>
2016-01-16 10:58:45 -0800

    client first attempt

--- src/apps/http_client.cc
+++ src/apps/http_client.cc
@@ -5,6 +5,7 @@
 #define BUFSIZE 1024
 
 int write_n_bytes(int fd, char * buf, int count);
+char *build_get_query(char *host, char *page);
 
 int main(int argc, char * argv[]) {
     char * server_name = NULL;
@@ -12,7 +13,7 @@ int main(int argc, char * argv[]) {
     char * server_path = NULL;
 
     int sock = 0;
-    int rc = -1;
+    int rc = -1; // I used this to store return values for each function.
     int datalen = 0;
     bool ok = true;
     struct sockaddr_in sa;
@@ -24,9 +25,12 @@ int main(int argc, char * argv[]) {
     char * bptr = NULL;
     char * bptr2 = NULL;
     char * endheaders = NULL;
+    char * getQuery=NULL;
    
     struct timeval timeout;
     fd_set set;
+
+
 
     /*parse args */
     if (argc != 5) {
@@ -51,21 +55,78 @@ int main(int argc, char * argv[]) {
     }
 
     /* create socket */
-
+    // Create a socket.  Type must be SOCK_STREAM (TCP)
+    // or SOCK_DGRAM (UDP)
+    // or SOCK_ICMP (ICMP)
+    sock=minet_socket (SOCK_STREAM);
     // Do DNS lookup
     /* Hint: use gethostbyname() */
+    struct hostent *hostentry;
+    if ((hostentry = gethostbyname(server_name)) == NULL) {  // get the host info
+        fprintf(stderr, "Failed to get host ip address at: %s\n",server_name);
+        exit(-1);
+    }
 
     /* set address */
-
+    // Specify ip address (AF_INET) and port for struct sockaddr_in sa;
+    sa.sin_family = AF_INET; //AF_INET means that src points to a character string containing an IPv4 network address in dotted-decimal format, "ddd.ddd.ddd.ddd"
+    //Another option would be AF_INET6, which stands for ipv6 network
+    sa.sin_port = htons(server_port);
+    //set the address for sa.
+    if (inet_pton(AF_INET,hostentry->h_addr, &sa.sin_addr.s_addr)<=0){
+        fprintf(stderr, "Can't set remote->sin_addr.s_addr. %s is not a valid IP address\n",hostentry->h_addr);
+        exit(-1);
+    }
     /* connect socket */
-    
+
+    // Connect to a remote socket.
+    // the IP address (AF_INET) and port are specified in myaddr.
+    // The sockfd argument is a file descriptor that refers to a socket of
+    // type SOCK_STREAM or SOCK_SEQPACKET.
+    minet_bind (SOCK_STREAM,sa);
+    if(minet_connect(sock, sa) < 0){
+        fprintf("Could not connect to socket");
+        exit(-1);
+    }
     /* send request */
+    // Write to a connected socket (returns # bytes actually written).
+    getQuery=build_get_query(server_name,server_path);
+    write_n_bytes(sock, getQuery, strlen(getQuery));
+  
 
     /* wait till socket can be read */
     /* Hint: use select(), and ignore timeout for now. */
+
     
     /* first read loop -- read headers */
-    
+    memset(buf, 0, sizeof(buf));
+      int htmlstart = 0;
+      char * htmlcontent;
+    while((rc = recv(sock, buf, BUFSIZ, 0)) > 0){
+    if(htmlstart == 0)
+    {
+      /* Under certain conditions this will not work.
+      * If the \r\n\r\n part is splitted into two messages
+      * it will fail to detect the beginning of HTML content
+      */
+      htmlcontent = strstr(buf, "\r\n\r\n");
+      if(htmlcontent != NULL){
+        htmlstart = 1;
+        htmlcontent += 4;
+      }
+    }else{
+      htmlcontent = buf;
+    }
+    if(htmlstart){
+      fprintf(stdout, htmlcontent);
+    }
+
+        memset(buf, 0, rc);
+    }
+    if(rc < 0)
+    {
+    perror("Error receiving data");
+    }
     /* examine return code */   
     //Skip "HTTP/1.0"
     //remove the '\0'
@@ -76,7 +137,8 @@ int main(int argc, char * argv[]) {
     /* second read loop -- print out the rest of the response */
     
     /*close socket and deinitialize */
-
+    free(getQuery);
+    minet_close(sock);
 
     if (ok) {
 	return 0;
@@ -101,3 +163,13 @@ int write_n_bytes(int fd, char * buf, int count) {
 }
 
 
+char *build_get_query(char *host, char *page){
+    // Useful little function
+    // modified from http://coding.debuntu.org/c-linux-socket-programming-tcp-simple-http-client
+    char* query;
+    char *tpl = "GET /%s HTTP/1.0\r\nHost: %s\r\nUser-Agent: %s\r\n\r\n";
+    // -5 is to consider the %s %s %s in tpl and the ending \0
+    query = (char *)malloc(strlen(host)+strlen(page)+strlen(USERAGENT)+strlen(tpl)-5);
+    sprintf(query, tpl, page, host, USERAGENT);
+    return query;
+}
