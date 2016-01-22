310d9c2a63c781c8237388c1e7d8c62b270b0a1e
Xun Zheng <xzh417@tlab-06.ece.northwestern.edu>
2016-01-22 17:15:54 -0600

    part3 workspace setup

--- src/apps/http_server2.cc
+++ src/apps/http_server2.cc
@@ -6,6 +6,7 @@
 
 #define BUFSIZE 1024
 #define FILENAMESIZE 100
+#define MAXFILESIZE 1024
 
 int handle_connection(int);
 int writenbytes(int,char *,int);
@@ -21,6 +22,8 @@ int main(int argc,char *argv[])
   fd_set connections;
   int maxfd;
 
+  int backlog= 10; //Set backlog to whatever number of waiting connections you want in the server.
+
   /* parse command line args */
   if (argc != 3)
   {
@@ -34,13 +37,59 @@ int main(int argc,char *argv[])
     exit(-1);
   }
 
+
+  // COPIED FROM http_server1.cc; MAY NEED MODIFICATION LATER
+
   /* initialize and make socket */
+  if (toupper(*(argv[1])) == 'K') { 
+  minet_init(MINET_KERNEL);
+  } else if (toupper(*(argv[1])) == 'U') { 
+  minet_init(MINET_USER);
+  } else {
+  fprintf(stderr, "First argument must be k or u\n");
+  exit(-1);
+  }
+
+  /* create socket */
+  // Type must be SOCK_STREAM (TCP)
+  // or SOCK_DGRAM (UDP)
+  // or SOCK_ICMP (ICMP)
+  sock=minet_socket (SOCK_STREAM);
+  //sock2=minet_socket (SOCK_STREAM);
+  
 
   /* set server address*/
+  // Specify ip address (AF_INET) and port for struct sockaddr_in sa;
+  sa.sin_family = AF_INET; //AF_INET means that src points to a character string containing an IPv4 network address in dotted-decimal format, "ddd.ddd.ddd.ddd"
+  //Another option would be AF_INET6, which stands for ipv6 network
+  sa.sin_port = htons(server_port);
+  //set the address for sa. let the program automatically select an IP address:
+  sa.sin_addr.s_addr = INADDR_ANY;
+
+  //sa2.sin_family = AF_INET;
+  //sa2.sin_port = htons(server_port);
+  //sa2.sin_addr.s_addr = INADDR_ANY;
+
 
   /* bind listening socket */
+  // The sockfd argument is a file descriptor that refers to a socket of
+  // type SOCK_STREAM or SOCK_SEQPACKET.
+  minet_bind (sock,&sa); 
+  /**
+   * TODO @@@@@@@@@@@ ATTENTION @@@@@@@@@@@
+   * I think this one above should be 'sock', instead of 'SOCK_STREAM'
+   * The following from minet_socket.cc:
+   * @param[in] sockfd Handle to an unbound socket that was created
+   * using minet_socket().
+   */
+
+  //minet_bind (sock2,&sa2);
+
 
   /* start listening */
+  minet_listen(sock,backlog);
+  //minet_listen(sock2,backlog2);
+
 
   /* connection handling loop */
   while(1)
@@ -51,6 +100,8 @@ int main(int argc,char *argv[])
 
     /* process sockets that are ready */
 
+    i = minet_accept(sock,&sa);
+
       /* for the accept socket, add accepted connection to connections */
       if (i == sock)
       {
@@ -60,11 +111,21 @@ int main(int argc,char *argv[])
 	rc = handle_connection(i);
       }
   }
+  minet_close(i);
 }
+
+
+
+
+// COPIED FROM http_server1.cc; MAY NEED MODIFICATION LATER
 
 int handle_connection(int sock2)
 {
+  char filecontent[MAXFILESIZE+1];
   char filename[FILENAMESIZE+1];
+
+  FILE *fp;
+  size_t pos = 0;
   int rc;
   int fd;
   struct stat filestat;
@@ -73,6 +134,7 @@ int handle_connection(int sock2)
   char *endheaders;
   char *bptr;
   int datalen=0;
+  size_t addresslength;
   char *ok_response_f = "HTTP/1.0 200 OK\r\n"\
                       "Content-type: text/plain\r\n"\
                       "Content-length: %d \r\n\r\n";
@@ -80,35 +142,114 @@ int handle_connection(int sock2)
   char *notok_response = "HTTP/1.0 404 FILE NOT FOUND\r\n"\
                          "Content-type: text/html\r\n\r\n"\
                          "<html><body bgColor=black text=white>\n"\
-                         "<h2>404 FILE NOT FOUND</h2>\n"\
+                         "<h2>404 FILE NOT FOUND</h2>\n"
                          "</body></html>\n";
   bool ok=true;
 
+
+
   /* first read loop -- get request and headers*/
+
+
+  memset(buf, 0, sizeof(buf));
+
+  //while((rc = minet_read(sock2, buf, BUFSIZE)) > 0){
+
+    // header: after first \r\n
+    // end of header: \r\n\r\n
+    
+    // The assumption was that if it can read one byte, 
+    // it can read everything. So I'm going to set header
+    // after this loop
+  //}
+  rc = minet_read(sock2, buf, BUFSIZE);
+
+
+  // I'll use bptr to denote the start of file address
+  bptr = buf + 5;
+  headers = strstr(buf, "\r\n") + 2; // +2 because header is after this token
+  endheaders = strstr(buf, "\r\n\r\n") + 4; // +4 so the end of headers is the end of string
+  
 
   /* parse request to get file name */
   /* Assumption: this is a GET request and filename contains no spaces*/
 
+  // char *tpl = "GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n";
+  // I assume that the first "%s" is the entirety of the file name
+  // the first '/' will be skipped
+
+  memset(filename, '\0', sizeof(filename));
+  // memset(filecontent, '\0', sizeof(filecontent));
+
+  addresslength = (int)(strstr(buf + 5, " ") - (buf + 5));
+  memcpy(filename, buf + 5, addresslength);
+
+
     /* try opening the file */
+  // we assume that this is not a directory request
+  // therefore not checking S_ISDIR(filestat.st_mode)
+  if ( stat( filename , &filestat ) < 0 ){
+    ok = false;
+  } else {
+
+    fp = fopen( filename, "r" );
+    
+    int ich;
+    while ( ( ich = getc( fp ) ) != EOF && pos < MAXFILESIZE) {
+      filecontent[pos] = (char)ich;
+      pos ++;
+    }
+    //filecontent[pos] = '\r';
+    //filecontent[pos + 1] = '\n';
+    //pos += 2;
+  }
 
   /* send response */
   if (ok)
   {
     /* send headers */
+    sprintf(ok_response,ok_response_f, pos - 1);
+    char output[MAXFILESIZE+1];
+    memset(output, '\0', sizeof(output));
+    size_t tempPos = 0;
+    size_t oksize = sizeof(ok_response);
+    for (size_t i = 0; ok_response[i] != '\0' ; i ++ ) {
+        output[tempPos] = ok_response[i];
+        tempPos ++;
+    }
+    //output[tempPos] = '\r';
+    //output[tempPos] = '\n';
+    //tempPos += 2;
+    for (size_t i = 0; i < pos ; i ++ ) {
+        output[tempPos] = filecontent[i];
+        tempPos ++;
+    }
 
+    //minet_write(sock2, ok_response, sizeof(ok_response));
+    minet_write(sock2, output, tempPos);//ok_response, sizeof(ok_response));
     /* send file */
+    //minet_write(sock2, filecontent, pos - 1);
   }
-  else	// send error response
+  else // send error response
   {
+    minet_write(sock2, notok_response, strlen(notok_response));
   }
 
   /* close socket and free space */
+  // no malloc or new; nothing to free or delete
+  minet_close(sock2);
+
 
   if (ok)
     return 0;
   else
     return -1;
 }
+
+
+
+
+
 
 int readnbytes(int fd,char *buf,int size)
 {
